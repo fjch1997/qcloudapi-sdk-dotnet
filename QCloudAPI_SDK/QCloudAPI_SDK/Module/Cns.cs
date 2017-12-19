419f10a7067c45a95d54b6c454c8a43ac626f750
Lloyd Feng <fjch1997@outlook.com>
2017-12-18 20:24:53 -0500

    Added cns.

---
+++ QCloudAPI_SDK/QCloudAPI_SDK/Module/Cns.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QCloudAPI_SDK.Module
+{
+    public class Cns : Base
+    {
+        public Cns()
+        {
+            serverHost = "cns.api.qcloud.com";
+        }
+    }
+}
